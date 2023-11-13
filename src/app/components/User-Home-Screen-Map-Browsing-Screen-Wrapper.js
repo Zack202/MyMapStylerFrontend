8364a2bb746651f6878d517c09b401b75c32d389
Zachary Loiwnger <zackxbl4@gmail.com>
2023-11-12 20:24:36 -0500

    Base Home/Browsing File Made

---
+++ src/app/components/User-Home-Screen-Map-Browsing-Screen-Wrapper.js
@@ -0,0 +1,18 @@
+
+
+// Import dependencies
+
+
+export default function UserHomeScreenMapBrowsingScreenWrapper() {
+
+    //code here for if user is on home screen or map browsing screen
+
+
+
+
+    return(
+        <div className={styles.container}>
+            \* code here for combined home/browser *\
+        </div>
+    )
+}
---
+++ src/app/components/index.js
@@ -0,0 +1,8 @@
+import { UserHomeScreenMapBrowsingScreenWrapper } from "./User-Home-Screen-Map-Browsing-Screen-Wrapper";
+
+
+
+
+export {
+    UserHomeScreenMapBrowsingScreenWrapper,
+}
