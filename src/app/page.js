75cc05416d23a4a33bf115fcfdc8be596b090476
s1rk-36 <hajredinajk@gmail.com>
2023-11-13 12:08:26 -0500

    Revert "build 2 commit"
    This reverts commit ea38d5bf5adbb5f84b26dd2c909a2ff2670b6c0e.

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,50 @@
 'use client'
+import Image from 'next/image'
 import styles from './page.module.css'
 import { useState } from 'react'
 import api from './api'
-import TopAppBanner from './components/TopAppBanner'
-import BottomAppBanner from './components/BottomAppBanner'
-import { BrowserRouter } from 'react-router-dom'
-import CreateMapModal from './components/CreateMapModal'
-import MapCard from './components/MapCard'
-import LeafletMapContainer from './components/LeafletMapContainer'
-import EditToolbar from './components/EditToolbar'
 
 
 export default function Home() {
+
+  const [names, setNames] = useState('')
+  const [name, setName] = useState('')
+
+  const handleAddName = async () => {
+    console.log("Handle add name", name)
+    if (name == '') {
+      return
+    }else{
+    const result = await api.createNewName(name)
+    console.log(result)
+    setName('')
+    document.getElementById("nameinput").value = "";
+    //handleGetNames()
+    }
+  }
+
+  const handleGetNames = async () => {
+    const result = await api.getNames()
+    const nameList = result.data.data.map(item => item.name);
+    console.log(nameList)
+    const namesWithCommas = nameList.join(', ');
+    setNames(namesWithCommas)
+  }
+
   
   return (
-    <BrowserRouter>
-      <TopAppBanner />
-      <EditToolbar />
-      <BottomAppBanner />
-    </BrowserRouter>
+    <main className={styles.main}>
+      <div className={styles.center}>
+        MyMapStyler
+      </div>
+      <button className={styles.button}>Login</button>
+      <button className={styles.button}>Create Account</button>
+
+      <input id="nameinput" className={styles.input} onChange={(e) => setName(e.target.value)} type="text" placeholder="Enter Name"></input>
+      <button className={styles.button} onClick={handleAddName}>Submit Name</button>
+      <h2 className={styles.h2}>All names: {names}</h2>
+      <button className={styles.button} onClick={handleGetNames}>Refresh all names from db</button>
+
+    </main>
   )
 }
