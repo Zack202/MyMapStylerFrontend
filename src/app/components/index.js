fbae32b63b95f74b472cdfe5cbca36d96f6844e3
Zachary Loiwnger <zackxbl4@gmail.com>
2023-11-13 13:26:57 -0500

    Added Michelle Comp to Routes

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,9 +1,15 @@
 import CreateAccountScreen from "./CreateAccountScreen";
 import SplashScreen from "./SplashScreen";
+import ProfileScreen from "./ProfileScreen";
+import LoginScreen from "./LoginScreen";
+import SpecificMapScreen from "./SpecificMapScreen";
 
 
 
 export {
-    CreateAccountScreen
-    SplashScreen
+    CreateAccountScreen,
+    SplashScreen,
+    ProfileScreen,
+    LoginScreen,
+    SpecificMapScreen,
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import { SplashScreen } from './components';
+import { LoginScreen, SplashScreen, ProfileScreen, SpecificMapScreen } from './components';
 import React from "react";
 
 import { CreateAccountScreen } from './components'
@@ -18,9 +18,25 @@ import { CreateAccountScreen } from './components'
 
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: "/f",
       element: <SplashScreen/>
-    }
+    },
+    {
+      path: "/d",
+      element: <CreateAccountScreen/>
+    },
+    {
+      path: "/a",
+      element: <LoginScreen/>
+    },
+    {
+      path: "/c",
+      element: <ProfileScreen/>
+    },
+    {
+      path: "/",
+      element: <SpecificMapScreen/>
+    },
   ]);
 
 export default function Home() {
