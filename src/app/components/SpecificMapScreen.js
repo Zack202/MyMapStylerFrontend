660276d23f7108451edd53ec34c56db2f0286a25
Zachary Loiwnger <zackxbl4@gmail.com>
2023-11-13 11:30:45 -0500

    Comments Done and Set up Specific Map Screen

---
+++ src/app/components/SpecificMapScreen.js
@@ -0,0 +1,14 @@
+import CommentSection from './CommentSection';
+
+
+
+
+
+
+export default function SpecificMapScreen() {
+    return(
+        <div className={styles.container}>
+            <CommentSection />
+        </div>
+    )
+}
---
+++ src/app/components/index.js
@@ -0,0 +1,12 @@
+import SpecificMapScreen from './SpecificMapScreen';
+import CommentSection from './CommentSection';
+
+
+
+
+
+
+export {
+    SpecificMapScreen,
+    CommentSection
+}
