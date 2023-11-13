aff6079c96f8012b3007913369b2b45e3d10246f
Zachary Loiwnger <zackxbl4@gmail.com>
2023-11-13 01:02:38 -0500

    ImportMapDataModal (Weird bug with button color)

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,9 +1,10 @@
 import CreateAccountScreen from "./CreateAccountScreen";
 import MapEditor from "./MapEditor";
-
+import ImportMapDataModal from "./ImportMapDataModal";
 
 
 export {
     CreateAccountScreen,
-    MapEditor
+    MapEditor,
+    ImportMapDataModal
 }
