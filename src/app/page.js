ba5e93581d1ac6f09ec87c28a6331857d4fe741f
Zachary Loiwnger <zackxbl4@gmail.com>
2023-11-13 20:38:56 -0500

    New Build Pack

--- src/app/page.js
+++ src/app/page.js
@@ -8,39 +8,6 @@ import React from "react";
 import Link from "next/link"
 import { CreateAccountScreen } from './components'
 
-/*
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <SplashScreen/>
-    },
-    {
-      path: "/d",
-      element: <CreateAccountScreen/>
-    },
-    {
-      path: "/a",
-      element: <LoginScreen/>
-    },
-    {
-      path: "/c",
-      element: <ProfileScreen/>
-    },
-    {
-      path: "/e",
-      element: <SpecificMapScreen/>
-    },
-    {
-      path: "/b",
-      element: <MapEditingScreen/>
-    },
-    {
-      path: "/f",
-      element: <UserHomeScreenMapBrowsingScreenWrapper/>
-    },
-  ]);
-  */
-
 export default function Home() {
   
   return (
