c71c34a4cb467c8c4b266b7da954595c13fec729
mlow <michelle.low.1@stonybrook.edu>
2023-11-14 12:37:43 -0500

    connect edit account info modal

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -17,9 +17,32 @@ import Container from '@mui/material/Container';
 import IconButton from '@mui/material/IconButton';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import BottomAppBanner from '../home_browser/BottomAppBanner';
+/////specifically from modal
+import Avatar from '@mui/material/Avatar';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Modal from '@mui/material/Modal';
+
+
+function Copyright(props) {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit" href="https://mui.com/">
+          MyMapStyler
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
 
 const defaultTheme = createTheme({
     palette: {
+        background: {
+            paper: '#fff',
+        },
         primary: {
             main: '#990000'
         },
@@ -30,6 +53,20 @@ const defaultTheme = createTheme({
 },);
 
 export default function Profile() {
+    //for modal
+    const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
     return (
         <div>
             <Grid item xs={12}>
@@ -107,7 +144,7 @@ export default function Profile() {
                                             <div>
                                                 <Grid container spacing={5} align="center">
                                                     <Grid item='true'>
-                                                        <Button variant="contained" color="primary">
+                                                        <Button onClick={handleOpen} variant="contained" color="primary">
                                                             Edit Account Information
                                                         </Button>
                                                     </Grid>
@@ -123,6 +160,107 @@ export default function Profile() {
                                     </Box>
                                 </Box>
                             </Container>
+                            <Modal
+        open={open}
+        onClose={handleClose}
+        >
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              bgcolor: 'background.paper',
+              p:2
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Change Account Information
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="family-name"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="username"
+                    label="Username"
+                    name="username"
+                    autoComplete="username"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="confirmpassword"
+                    label="Confirm Password"
+                    type="confirmpassword"
+                    id="confirmpassword"
+                    autoComplete="confirm-password"
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Save
+              </Button>
+            </Box>
+          </Box>
+          <Copyright sx={{ mt: 5 }} />
+        </Container>
+          </Modal>
                         </div>
                     </main>
                 </ThemeProvider>
