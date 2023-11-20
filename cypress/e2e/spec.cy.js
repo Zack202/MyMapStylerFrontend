ea719a0120ea7864ea78cd9b51dbca54adeacfbb
Zack202 <zackxbl4@gmail.com>
2023-11-20 17:31:16 -0500

    final test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 beforeEach(() => {
   cy.viewport(1200, 750)
-  cy.visit("http://localhost:3000")
+  cy.visit("https://my-map-styler-frontend-60bea3c51be3.herokuapp.com/")
 })
 describe('Guest Component pathing Test', () => {
 
@@ -10,7 +10,7 @@ describe('Guest Component pathing Test', () => {
   });
 
   it('Check Login', ()=> {
-    cy.visit("http://localhost:3000/login")
+    cy.visit("https://my-map-styler-frontend-60bea3c51be3.herokuapp.com/login")
     /* ==== Generated with Cypress Studio ==== */
     cy.get('#email').clear('E');
     cy.get('#email').type('Email');
@@ -23,7 +23,7 @@ describe('Guest Component pathing Test', () => {
   /* ==== Test Created with Cypress Studio ==== */
   it('Create Account', function() {
     /* ==== Generated with Cypress Studio ==== */
-    cy.visit('http://localhost:3000/createAccount');
+    cy.visit('https://my-map-styler-frontend-60bea3c51be3.herokuapp.com/createAccount');
     cy.get('#firstName').clear();
     cy.get('#firstName').type('First Name');
     cy.get(':nth-child(2) > .MuiFormControl-root').click();
@@ -43,8 +43,8 @@ describe('Guest Component pathing Test', () => {
   /* ==== Test Created with Cypress Studio ==== */
   it('Guest User', function() {
     /* ==== Generated with Cypress Studio ==== */
-    cy.visit('http://localhost:3000/');
-    cy.visit('http://localhost:3000/home_browser')
+    cy.visit('https://my-map-styler-frontend-60bea3c51be3.herokuapp.com/');
+    cy.visit('https://my-map-styler-frontend-60bea3c51be3.herokuapp.com/home_browser')
     cy.get('.css-14j5k7k').click();
     /* ==== End Cypress Studio ==== */
   });
